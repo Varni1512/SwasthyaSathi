aef5d6b55fe5d398da57346225d6e4a67c0b5c66
Varni1102 <varnikumar1512@gmail.com>
2025-09-07 23:16:31 +0530

    updated

--- app/(routes)/dashboard/_components/Header.tsx
+++ app/(routes)/dashboard/_components/Header.tsx
@@ -24,7 +24,7 @@ const menuOptions = [
 const Header = () => {
     return (
         <div className='flex items-center justify-between p-4 shadow px-10 md:px-20 lg:px-40'>
-            <Image src={'/logo.svg'} alt="Logo" width={180} height={90} />
+            <Image src={'/logo.png'} alt="Logo" width={180} height={90} />
             <div className='hidden md:flex gap-12 items-center'>
                 {menuOptions.map((option, index) => (
                     <div key={index}>
--- app/page.tsx
+++ app/page.tsx
@@ -72,31 +72,6 @@ export default function HeroSectionOne() {
             Explore Now
           </button>
         </motion.div>
-        {/* <motion.div
-          initial={{
-            opacity: 0,
-            y: 10,
-          }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
-          transition={{
-            duration: 0.3,
-            delay: 1.2,
-          }}
-          className="relative z-10 mt-20 rounded-3xl border border-neutral-200 bg-neutral-100 p-4 shadow-md dark:border-neutral-800 dark:bg-neutral-900"
-        >
-          <div className="w-full overflow-hidden rounded-xl border border-gray-300 dark:border-gray-700">
-            <img
-              src="https://assets.aceternity.com/pro/aceternity-landing.webp"
-              alt="Landing page preview"
-              className="aspect-[16/9] h-auto w-full object-cover"
-              height={1000}
-              width={1000}
-            />
-          </div>
-        </motion.div> */}
       </div>
     </div>
   );
